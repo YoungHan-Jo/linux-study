bc6bc2a988e5cd076a1f108d2110b534396cbe2f
younghan-jo <verin4494@gmail.com>
2023-03-30 19:20:34 +0900

    bc sed % #

---
+++ shell/sed/prac10-2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+while [ $# -gt 0 ]; do
+	src=$1
+	shift
+
+	name=${src%.*}
+	ext=${src##*.}
+	dst=$name.jpeg
+	
+	if [ $ext = jpg ]; then
+		echo rename $src to $dst
+		mv $src $dst
+	fi
+done
+
+
