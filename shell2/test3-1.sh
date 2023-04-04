c346dc6de80f994010f30a4229755c6bc82252b0
younghan-jo <verin4494@gmail.com>
2023-04-04 23:55:24 +0900

    if elif case

---
+++ shell2/test3-1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ $1 = malt ]; then
+	echo Argument is malt
+elif [ $1 = wheat ]; then
+	echo Argument is weat
+else
+	echo Arguemtn is neither malt nor wheat
+fi
+
+
---
+++ shell2/test3-3.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+case $1 in
+	wheat) echo Bread ;;
+	barley) echo Beer ;;
+	*) echo Nothing ;;
+esac
+
